9058df136b12ceb831fb82154b394b5d5e52c4e7
kraithus <xordkraithus@gmail.com>
2023-12-12 07:04:59 +0200

    Works but I don't understand why
    This works using the for loop. Simply converting duration to milliseconds but idk why I have to do this I am not using pos anywhere in my code

--- main.py
+++ main.py
@@ -16,29 +16,37 @@ while not player.core_idle:
     time.sleep(0.5)
     overlay = player.create_image_overlay()
     duration = player.duration
+    durtaionToMilliseconds = int(duration*100)
 
-    # ts is current video time
-    ts = player.time_pos
-    if ts is None:
-        break
+    for pos in range(0, durtaionToMilliseconds, 5):
+        # ts is current video time
+        ts = player.time_pos
+        if ts is None:
+            break
 
-    # Colours for the text we will draw, yes colours
-    img = Image.new('RGBA', (400, 150),  (255, 255, 255, 0))
-    d = ImageDraw.Draw(img)
+        # Colours for the text we will draw, yes colours
+        img = Image.new('RGBA', (400, 150),  (255, 255, 255, 0))
+        d = ImageDraw.Draw(img)
 
-    # What to show depending on the timestamp e.g. x < ts < y reads as
-    # current time greater than 2 but less than 4 do foo
-    if 2 < ts < 4:
-        d.text((10, 10), 'ITZY', font=font, fill=(0, 255, 255, 128))
-        d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
-    if 5 < ts < 9:
-        d.text((10, 10), 'Kweenka', font=font, fill=(0, 255, 255, 128))
-        d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
-    if 10 < ts < 15:
-        d.text((10, 10), 'Test-San', font=font, fill=(0, 255, 255, 128))
-        d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
-
-    # I have no fucking idea don't ask me
-    overlay.update(img)
-    time.sleep(0.05)
+        # What to show depending on the timestamp e.g. x < ts < y reads as
+        # current time greater than 2 but less than 4 do foo
+        if 2 < ts < 4:
+            d.text((10, 10), 'ITZY', font=font, fill=(0, 255, 255, 128))
+            d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
+        if 5 < ts < 9:
+            d.text((10, 10), 'Kweenka', font=font, fill=(0, 255, 255, 128))
+            d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
+        if 10 < ts < 15:
+            d.text((10, 10), 'Test-San', font=font, fill=(0, 255, 255, 128))
+            d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
+        if 17 < ts < 23:
+            d.text((10, 10), 'Baka-Sama', font=font, fill=(0, 255, 255, 128))
+            d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
+        if 178 < ts < 180:
+            d.text((10, 10), 'Baka-Sama', font=font, fill=(0, 255, 255, 128))
+            d.text((10, 60), f'{duration:.3f}', font=font, fill=(0, 255, 255, 128))
+            
+        # I have no fucking idea don't ask me
+        overlay.update(img)
+        time.sleep(0.05)
 
